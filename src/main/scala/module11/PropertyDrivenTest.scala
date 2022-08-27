ec4c002c9326132bce273d20cd45c6b2515eb469
obaid147 <obaidfayazwani@gmail.com>
2022-08-27 13:43:30 +0530

    Property driven tests and customer Generator with it

--- src/main/scala/module11/PropertyDrivenTest.scala
+++ src/main/scala/module11/PropertyDrivenTest.scala
@@ -1,10 +1,24 @@
 import org.scalacheck.Gen
 import org.scalatest.{FunSpec, Matchers}
+import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
 
-import scala.xml.Properties
+class PropertyDrivenTest extends FunSpec with Matchers with ScalaCheckPropertyChecks{
+  /**
+   * Scalatest has support for Scalacheck, which can generate random testing data: */
 
-class PropertyDrivenTest extends FunSpec with Matchers {
-  val validChars = ('a' to 'z') ++ ('A' to 'Z') ++ (0 to 9)
+  describe("Property Checks") {
+    they("should ensure absolute on all Ints returns a positive number") {
+      forAll{
+        (i: Int) =>
+          whenever(i != Int.MinValue) {
+            i.abs should be >= 0
+          }
+      }
+    }
+  }
+
+  // Custom Property Generators
+  val validChars = ('a' to 'z') ++ ('A' to 'Z') ++ ('0' to '9')
 
   val char = Gen.oneOf(validChars)
 
@@ -20,6 +34,13 @@ class PropertyDrivenTest extends FunSpec with Matchers {
         println(str)
         str.reverse.reverse should be (str)
       }
+    }
+
+    it("should work on empty strings"){
+      "".reverse should be ("")
+    }
+    it("should actually reverse a string"){
+      "obaid".reverse should be("diabo")
     }
   }
 
